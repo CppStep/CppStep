ba2f3a53ac394cbb22324909ec6a126245403de0
Jonathan Tanner <nixCodeX@users.noreply.github.com>
2018-11-12 16:15:52 +0000

    Show push progress

--- genDocs.sh
+++ genDocs.sh
@@ -19,4 +19,4 @@ cd docs/html
 touch .nojekyll
 
 git commit -m "Deploy code docs to GitHub Pages Travis build: ${TRAVIS_BUILD_NUMBER}" -m "Commit: ${TRAVIS_COMMIT}"
-git push --force "https://${GH_REPO_TOKEN}@${GH_REPO_REF}" > /dev/null 2>&1
+git push --force "https://${GH_REPO_TOKEN}@${GH_REPO_REF}"
