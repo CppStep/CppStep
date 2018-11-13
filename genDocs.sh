c3799ca562b86fc8102f70e6d9dbcb7d2c735514
Jonathan Tanner <nixCodeX@users.noreply.github.com>
2018-11-13 13:02:25 +0000

    Configure git in the right directory

--- genDocs.sh
+++ genDocs.sh
@@ -5,18 +5,18 @@ set -e
 # Get the current gh-pages branch
 git clone -b gh-pages https://git@$GH_REPO_REF docs
 
+doxygen
+
+cd docs
+
+touch .nojekyll
+
 ##### Configure git.
 # Set the push default to simple i.e. push only the current branch.
 git config --global push.default simple
 # Pretend to be an user called Travis CI.
 git config user.name "Travis CI"
 git config user.email "travis@travis-ci.org"
-
-doxygen
-
-cd docs
-
-touch .nojekyll
 
 git add .
 git commit -m "Deploy code docs to GitHub Pages Travis build: ${TRAVIS_BUILD_NUMBER}" -m "Commit: ${TRAVIS_COMMIT}"
